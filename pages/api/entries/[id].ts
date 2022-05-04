b3a069cdb380488b65582e6beb86a70933fc1d23
wedburst <jhonrawed@gmail.com>
2022-05-04 17:54:55 -0500

    update entries in backend

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,65 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import mongoose from "mongoose";
+import { db } from "../../../database";
+import { Entry, IEntry } from "../../../models";
+
+type Data = { message: string } | IEntry;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // console.log(req.query);
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: "ID no es válido " + id });
+  }
+
+  switch (req.method) {
+    case "PUT":
+      return updateEntry(req, res);
+
+    default:
+      return res.status(400).json({ message: "Endpoint no existe" });
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+
+  await db.connect();
+
+  const entryToUpdate = await Entry.findById(id);
+
+  if (!entryToUpdate) {
+    await db.disconnect();
+    return res
+      .status(400)
+      .json({ message: "No hay entradas con este ID: " + id });
+  }
+
+  const {
+    description = entryToUpdate.description,
+    status = entryToUpdate.status,
+  } = req.body;
+
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      { description, status },
+      { runValidators: true, new: true }
+    );
+    await db.disconnect();
+    return res.status(200).json(updatedEntry!);
+  } catch (error) {
+    await db.disconnect();
+    console.log({error});
+    res.status(400).json({ message: "Error al actualizar entrada, BAD REQUEST" });
+  }
+
+  // entryToUpdate.description = description;
+  // entryToUpdate.status = status;
+  // await entryToUpdate.save();
+
+};
