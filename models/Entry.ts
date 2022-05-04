29afd81f1cf0cb69a917eee5a092d75345233529
wedburst <jhonrawed@gmail.com>
2022-05-04 16:50:01 -0500

    creando entradas desde el backend

--- models/Entry.ts
+++ models/Entry.ts
@@ -1,19 +1,23 @@
-import mongoose, { Model, Schema } from 'mongoose';
-import { Entry } from '../interfaces';
+import mongoose, { Model, Schema } from "mongoose";
+import { Entry } from "../interfaces";
 
-export interface IEntry extends Entry{}
+export interface IEntry extends Entry {}
 
 const EntrySchema = new Schema({
-    description: { type: String, required: true },
-    createAt: { type: Number },
-    status: { 
-        type: String,
-        enum: ['pending', 'in-progress', 'finished'],
-        message: '{VALUE} no es un estado válido',
-     },
+  description: { type: String, required: true },
+  createAt: { type: Number },
+  status: {
+    type: String,
+    enum: {
+      values: ["pending", "in-progress", "finished"],
+      message: "{VALUE} no es un estado válido",
+    },
+    default: "pending",
+  },
 });
 
-const EntryModel: Model<IEntry> = mongoose.models.Entry || mongoose.model('Entry', EntrySchema);
+const EntryModel: Model<IEntry> =
+  mongoose.models.Entry || mongoose.model("Entry", EntrySchema);
 
 const entry = new EntryModel();
 
--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '../../../database';
 import { Entry, IEntry } from '../../../models';
 
@@ -11,14 +11,18 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
  
     switch (req.method) {
         case 'GET':
-            return getEntries(res)
+            return getEntries(res);
+
+        case 'POST':
+            return createEntry(req, res);
+
         default:
             return res.status(400).json({ message: 'Endpoint no existe' });
     }
 
 }
 
-const getEntries = async( res: NextApiRequest<Data>) => {
+const getEntries = async( res: NextApiResponse<Data>) => {
     await db.connect();
 
     const entries = await Entry.find().sort({
@@ -28,4 +32,32 @@ const getEntries = async( res: NextApiRequest<Data>) => {
     await db.disconnect();
 
     res.status(200).json(entries);
+}
+
+const createEntry = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { description = ''} = req.body;
+    
+    const newEntry = new Entry({
+        description,
+        createdAt: Date.now(),
+    });
+
+    try {
+        await db.connect();
+        await newEntry.save();
+        await db.disconnect();
+
+        return res.status(201).json(newEntry);
+
+    } catch (error) {
+        await db.disconnect();
+
+        console.log(error);
+
+        return res.status(400).json({ message: 'Error al crear entrada' });
+
+    }
+
+
 }
